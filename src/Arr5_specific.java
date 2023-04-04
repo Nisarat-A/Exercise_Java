57d3a0f997054d1d4d7eeb183503fa2753f560aa
Nisarat-A <noansrnn@gmail.com>
2023-04-04 13:17:01 +0700

    -

--- src/Arr5_specific.java
+++ src/Arr5_specific.java
@@ -1,2 +1,18 @@
 public class Arr5_specific {
+    public static boolean containers(int[] arr , int item){
+        for (int n: arr){
+            if (n == item) {
+                return true;
+            }
+        }return false;
+    }
+
+    public static void main(String[] args) {
+        int[] myarr_1 = { 1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2265, 1457, 2456};
+        System.out.println(containers(myarr_1,6549));
+        System.out.println(containers(myarr_1,2456));
+
+    }
 }
