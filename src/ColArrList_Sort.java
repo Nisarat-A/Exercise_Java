905c4b90a0497aec349a3584e470a99d7df6d4f0
Nisarat-A <noansrnn@gmail.com>
2023-04-04 20:43:28 +0700

    -

--- src/ColArrList_Sort.java
+++ src/ColArrList_Sort.java
@@ -1,6 +1,15 @@
-import java.util.ArrayList;
+import java.util.*;
 public class ColArrList_Sort {
     public static void main(String[] args) {
         ArrayList<String> color = new ArrayList<String>();
+        color.add("Red");
+        color.add("Green");
+        color.add("Orange");
+        color.add("White");
+        color.add("Black");
+        System.out.println("before sort " +color);
+        Collections.sort(color);
+        System.out.println("after sort " +color);
+
     }
 }
