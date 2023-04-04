db868bda382be6b3b2ecc33239cc0060b8264cfe
Nisarat-A <noansrnn@gmail.com>
2023-04-04 16:16:47 +0700

    -

--- src/Arr8.java
+++ src/Arr8.java
@@ -2,7 +2,12 @@ import java.util.Arrays;
 public class Arr8 {
     public static void main(String[] args) {
         int[] myarr = {1,2,3,4,5};
-        int [] new 
-
+        int [] new_arr = new int[5];
+        Arrays.sort(myarr);
+        System.out.println(Arrays.toString(myarr));
+    for (int i = 0 ; i<myarr.length;i++) {
+        new_arr[i] = myarr[i];}
+        System.out.println(Arrays.toString(new_arr));
     }
+
 }
