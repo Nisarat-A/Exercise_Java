cf64599f6ce7d6ec870a3d7fdbfc50b97f5fd350
Nisarat-A <noansrnn@gmail.com>
2023-04-04 13:26:31 +0700

    -

---
+++ src/Arr6.java
@@ -0,0 +1,12 @@
+public class Arr6 {
+    public static int findmyIndex(int[] myarr , int num){
+        if (myarr == null) return -1;
+        int len = myarr.length;
+        int i =0 ;
+        while (i<len){
+            if (myarr[i] == num){
+                
+            }
+        }
+    }
+}
